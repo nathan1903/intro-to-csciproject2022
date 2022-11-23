a06fa314cac28a240c91da917b1144ae5d937532
nathan1903 <115734411+nathan1903@users.noreply.github.com>
2022-11-22 23:33:46 -0600

    Add files via upload

---
+++ game-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox(int64_value, bool_value, string_value) VALUES (3120398, 1, 'whats up');
+
+UPDATE sandbox SET double_value = 9487283 WHERE id = 120;
+
+--DELETE sandbox WHERE id = 102
+
+SELECT * FROM sandbox;
